0d449d9c3db14b41cb790f93efc541ac1d1d11a6
Loïc Renaud <loic1.renaud@hotmail.fr>
2022-06-23 11:33:20 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import io from "socket.io-client";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const CONNECTION_URL = "wss://api.dev.stories.studio/";
+
+const connectSocket = () => {
+    const socket = io(CONNECTION_URL, {
+        transport: ["websocket"],
+        path: "/interview-test"
+    });
+    socket.onAny((...args) => {
+        console.log(...args);
+    });
+
+    return socket;
+};
+
+const App = () => {
+    return <div>JOIN Interview </div>;
+};
 
 export default App;
